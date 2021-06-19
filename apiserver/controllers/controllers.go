7f7671c71cd4964654b8b7d271a05568957bc1cd
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2021-06-19 15:01:33 +0000

    Cleanup commented code

--- apiserver/controllers/controllers.go
+++ apiserver/controllers/controllers.go
@@ -10,7 +10,7 @@ import (
 	"github.com/pkg/errors"
 
 	"coriolis-veeam-bridge/apiserver/params"
-	gErrors "coriolis-veeam-bridge/errors"
+	vErrors "coriolis-veeam-bridge/errors"
 	"coriolis-veeam-bridge/manager"
 )
 
@@ -70,12 +70,12 @@ func (a *APIController) CreateSnapStoreHandler(w http.ResponseWriter, r *http.Re
 	// CreateSnapStore
 	var newSnapData params.CreateSnapStoreRequest
 	if err := json.NewDecoder(r.Body).Decode(&newSnapData); err != nil {
-		handleError(w, gErrors.ErrBadRequest)
+		handleError(w, vErrors.ErrBadRequest)
 		return
 	}
 
 	if newSnapData.SnapStoreLocation == "" || newSnapData.TrackedDisk == "" {
-		handleError(w, gErrors.ErrBadRequest)
+		handleError(w, vErrors.ErrBadRequest)
 		return
 	}
 
@@ -114,16 +114,16 @@ func handleError(w http.ResponseWriter, err error) {
 	}
 
 	switch origErr.(type) {
-	case *gErrors.NotFoundError:
+	case *vErrors.NotFoundError:
 		w.WriteHeader(http.StatusNotFound)
 		apiErr.Error = "Not Found"
-	case *gErrors.UnauthorizedError:
+	case *vErrors.UnauthorizedError:
 		w.WriteHeader(http.StatusUnauthorized)
 		apiErr.Error = "Not Authorized"
-	case *gErrors.BadRequestError:
+	case *vErrors.BadRequestError:
 		w.WriteHeader(http.StatusBadRequest)
 		apiErr.Error = "Bad Request"
-	case *gErrors.ConflictError:
+	case *vErrors.ConflictError:
 		w.WriteHeader(http.StatusConflict)
 		apiErr.Error = "Conflict"
 	default:
@@ -134,183 +134,6 @@ func handleError(w http.ResponseWriter, err error) {
 
 	json.NewEncoder(w).Encode(apiErr)
 }
-
-// // GetVMHandler gets information about a single VM.
-// func (a *APIController) GetVMHandler(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	vmID, ok := vars["vmID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-// 	vmInfo, err := a.mgr.GetVirtualMachine(vmID)
-// 	if err != nil {
-// 		log.Printf("failed to get virtual machines: %q", err)
-// 		handleError(w, err)
-// 		return
-// 	}
-// 	json.NewEncoder(w).Encode(vmInfo)
-// }
-
-// // ListSnapshotsHandler lists all snapshots for a VM.
-// func (a *APIController) ListSnapshotsHandler(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	vmID, ok := vars["vmID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	snaps, err := a.mgr.ListSnapshots(vmID)
-// 	if err != nil {
-// 		log.Printf("failed to list snapshots: %q", err)
-// 		handleError(w, err)
-// 		return
-// 	}
-// 	json.NewEncoder(w).Encode(snaps)
-// }
-
-// // GetSnapshotHandler gets information about a single snapshot for a VM. It takes an optional
-// // query arg diff, which allows comparison of current snapshot, with a previous snapshot.
-// // The snapshot we are comparing to must exist and must be older than the current one.
-// func (a *APIController) GetSnapshotHandler(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	vmID, ok := vars["vmID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-// 	snapID, ok := vars["snapshotID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	squashChunksParam := r.URL.Query().Get("squashChunks")
-// 	var squashChunks bool
-// 	if squashChunksParam == "" {
-// 		// Default to true
-// 		squashChunks = true
-// 	} else {
-// 		squashChunks, _ = strconv.ParseBool(squashChunksParam)
-// 	}
-
-// 	compareTo := r.URL.Query().Get("compareTo")
-// 	snapshot, err := a.mgr.GetSnapshot(vmID, snapID, compareTo, squashChunks)
-// 	if err != nil {
-// 		log.Printf("failed to get snapshot: %q", err)
-// 		handleError(w, err)
-// 		return
-// 	}
-// 	json.NewEncoder(w).Encode(snapshot)
-// }
-
-// // DeleteSnapshotHandler removes one snapshot associated with a VM.
-// func (a *APIController) DeleteSnapshotHandler(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	vmID, ok := vars["vmID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-// 	snapID, ok := vars["snapshotID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	err := a.mgr.DeleteSnapshot(vmID, snapID)
-// 	if err != nil {
-// 		log.Printf("failed to delete snapshot: %q", err)
-// 		handleError(w, err)
-// 		return
-// 	}
-// 	w.WriteHeader(http.StatusOK)
-// }
-
-// // PurgeSnapshotsHandler deletes all snapshots associated with a VM.
-// func (a *APIController) PurgeSnapshotsHandler(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	vmID, ok := vars["vmID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-// 	if err := a.mgr.PurgeSnapshots(vmID); err != nil {
-// 		log.Printf("failed to purge snapshots: %q", err)
-// 		handleError(w, err)
-// 	}
-// 	w.WriteHeader(http.StatusOK)
-// }
-
-// // CreateSnapshotHandler creates a snapshots for a VM.
-// func (a *APIController) CreateSnapshotHandler(w http.ResponseWriter, r *http.Request) {
-// 	// CreateSnapshot
-// 	vars := mux.Vars(r)
-// 	vmID, ok := vars["vmID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	snapData, err := a.mgr.CreateSnapshot(vmID)
-// 	if err != nil {
-// 		log.Printf("failed to create snapshot: %q", err)
-// 		handleError(w, err)
-// 		return
-// 	}
-// 	json.NewEncoder(w).Encode(snapData)
-// }
-
-// // ConsumeSnapshotHandler allows the caller to download arbitrary ranges of disk data from a
-// // disk snapshot.
-// func (a *APIController) ConsumeSnapshotHandler(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	vmID, ok := vars["vmID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-// 	snapID, ok := vars["snapshotID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	diskID, ok := vars["diskID"]
-// 	if !ok {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	snapshot, err := a.mgr.GetSnapshot(vmID, snapID, "", false)
-// 	if err != nil {
-// 		log.Printf("failed to get snapshot: %q", err)
-// 		handleError(w, err)
-// 		return
-// 	}
-
-// 	var disk params.DiskSnapshot
-// 	for _, val := range snapshot.Disks {
-// 		if val.Name == diskID {
-// 			disk = val
-// 		}
-// 	}
-
-// 	if disk.Name == "" {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	fp, err := os.Open(disk.Path)
-// 	if err != nil {
-// 		log.Printf("failed open snapshot file: %q", err)
-// 		w.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
-// 	defer fp.Close()
-// 	http.ServeContent(w, r, disk.Path, time.Time{}, fp)
-// }
 
 // NotFoundHandler is returned when an invalid URL is acccessed
 func (a *APIController) NotFoundHandler(w http.ResponseWriter, r *http.Request) {
